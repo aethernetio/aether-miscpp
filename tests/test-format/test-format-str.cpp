9d8365472c10b753efa27898c80eee8e4e925480
BartolomeyKant <notevill@gmail.com>
2026-07-09 18:07:16 +0500

    update formatters

--- tests/test-format/test-format-str.cpp
+++ tests/test-format/test-format-str.cpp
@@ -16,14 +16,58 @@
 
 #include <unity.h>
 
+#include <array>
+#include <sstream>
+#include <string>
+#include <string_view>
+
 #include "aether-miscpp/format/format.h"
 
 namespace ae::test_format_str {
+
+static_assert(FormatScheme::kMaxFormatParts == 21);
+
+constexpr auto kConstexprScheme = FormatScheme{"{} {{}}"};
+static_assert(kConstexprScheme.source.size() == 7);
+static_assert(kConstexprScheme.part_count == 4);
+static_assert(!kConstexprScheme.overflow);
+static_assert(kConstexprScheme.parts[0].placeholder);
+static_assert(!kConstexprScheme.parts[1].placeholder);
+
+constexpr auto kEnvFormatScheme = FormatScheme{
+    "Platform:{}\n"
+    "Compiler:{}\n"
+    "Compiler version:{}\n"
+    "Library version:{}\n"
+    "Api version:{}\n"
+    "CPU arch:{}\n"
+    "Endianness:{}\n"
+    "UTMid:{}\n"};
+constexpr auto kEnvPlaceholderOffsets =
+    std::array<std::size_t, 8>{9, 21, 41, 60, 75, 87, 101, 110};
+
+constexpr bool EnvPlaceholdersMatch() {
+  for (std::size_t i = 0; i < kEnvPlaceholderOffsets.size(); ++i) {
+    auto const part = kEnvFormatScheme.parts[(i * 2) + 1];
+    if (!part.placeholder || part.offset != kEnvPlaceholderOffsets[i] ||
+        part.size != 2 || kEnvFormatScheme.source[part.offset] != '{' ||
+        kEnvFormatScheme.source[part.offset + 1] != '}') {
+      return false;
+    }
+  }
+  return true;
+}
+
+static_assert(kEnvFormatScheme.source.size() == 113);
+static_assert(kEnvFormatScheme.part_count == 17);
+static_assert(!kEnvFormatScheme.overflow);
+static_assert(EnvPlaceholdersMatch());
+
 void test_BracketInTheBegin() {
   auto str1 = Format("{ kek");
   TEST_ASSERT_EQUAL_STRING("{ kek", str1.c_str());
   auto str2 = Format("{} kek");
-  TEST_ASSERT_EQUAL_STRING(" kek", str2.c_str());
+  TEST_ASSERT_EQUAL_STRING("{} kek", str2.c_str());
 
   auto str3 = Format("} kek {");
   TEST_ASSERT_EQUAL_STRING("} kek {", str3.c_str());
@@ -33,7 +77,7 @@ void test_BracketInTheEnd() {
   auto str1 = Format("kek {");
   TEST_ASSERT_EQUAL_STRING("kek {", str1.c_str());
   auto str2 = Format("kek {}");
-  TEST_ASSERT_EQUAL_STRING("kek ", str2.c_str());
+  TEST_ASSERT_EQUAL_STRING("kek {}", str2.c_str());
 }
 
 void test_BracketEscape() {
@@ -41,10 +85,102 @@ void test_BracketEscape() {
   TEST_ASSERT_EQUAL_STRING("kek {}", str1.c_str());
 
   auto str2 = Format("{} kek {{}}");
-  TEST_ASSERT_EQUAL_STRING(" kek {}", str2.c_str());
+  TEST_ASSERT_EQUAL_STRING("{} kek {}", str2.c_str());
 
-  auto str3 = Format("{} kek {{}}", 12, 42);
+  auto str3 = Format("{} kek {{{}}}", 12, 42);
   TEST_ASSERT_EQUAL_STRING("12 kek {42}", str3.c_str());
+
+  auto str4 = Format("{{}}");
+  TEST_ASSERT_EQUAL_STRING("{}", str4.c_str());
+
+  auto str5 = Format("{{}");
+  TEST_ASSERT_EQUAL_STRING("{}", str5.c_str());
+
+  auto str6 = Format("{}}", 1);
+  TEST_ASSERT_EQUAL_STRING("1}", str6.c_str());
+
+  auto str7 = Format("{ {{");
+  TEST_ASSERT_EQUAL_STRING("{ {{", str7.c_str());
+
+  auto str8 = Format("}} {");
+  TEST_ASSERT_EQUAL_STRING("} {", str8.c_str());
+
+  auto str9 = Format("x { y {{ z", 123);
+  TEST_ASSERT_EQUAL_STRING("x { y {{ z", str9.c_str());
+}
+
+void test_MissingArgsAndLiteralNul() {
+  auto str = Format(std::string_view{"a{b}c"});
+  TEST_ASSERT_EQUAL_UINT32(5, static_cast<unsigned>(str.size()));
+  TEST_ASSERT_EQUAL_STRING("a{b}c", str.c_str());
+
+  auto option_missing_arg = Format("{:time}");
+  TEST_ASSERT_EQUAL_STRING("{:time}", option_missing_arg.c_str());
+
+  auto lit = Format("abc\0def");
+  TEST_ASSERT_EQUAL_UINT32(7, static_cast<unsigned>(lit.size()));
+  TEST_ASSERT_EQUAL_CHAR('a', lit[0]);
+  TEST_ASSERT_EQUAL_CHAR('b', lit[1]);
+  TEST_ASSERT_EQUAL_CHAR('c', lit[2]);
+  TEST_ASSERT_EQUAL_CHAR('\0', lit[3]);
+  TEST_ASSERT_EQUAL_CHAR('d', lit[4]);
+  TEST_ASSERT_EQUAL_CHAR('e', lit[5]);
+  TEST_ASSERT_EQUAL_CHAR('f', lit[6]);
+}
+
+void test_FormatTo() {
+  std::string out{"x"};
+  FormatTo(out, "-{}-", 42);
+  TEST_ASSERT_EQUAL_STRING("x-42-", out.c_str());
+
+  std::ostringstream oss;
+  FormatTo(oss, "{}:{}", 1, 2);
+  TEST_ASSERT_EQUAL_STRING("1:2", oss.str().c_str());
+}
+
+void test_FormatSchemeRuntimeAndOverflow() {
+  constexpr auto scheme = FormatScheme{"{} {{}}"};
+  static_assert(scheme.source.size() == 7);
+  TEST_ASSERT_EQUAL_STRING("1 {}", Format(scheme, 1).c_str());
+
+  auto runtime_format = std::string{"{}:{}"};
+  auto runtime = Format(std::string_view{runtime_format}, 1, 2);
+  TEST_ASSERT_EQUAL_STRING("1:2", runtime.c_str());
+
+  auto many = std::string{};
+  for (auto i = 0; i < 70; ++i) {
+    many += "{}";
+  }
+  auto overflow = Format(std::string_view{many}, 1, 2, 3);
+  auto expected = many + " OVERFLOW";
+  TEST_ASSERT_EQUAL_STRING(expected.c_str(), overflow.c_str());
+}
+
+void test_FormatSchemeTenPlaceholdersFit() {
+  constexpr auto scheme = FormatScheme{"a{}b{}c{}d{}e{}f{}g{}h{}i{}j{}k"};
+  static_assert(scheme.part_count == 21);
+  static_assert(!scheme.overflow);
+
+  auto result = Format(scheme, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+  TEST_ASSERT_EQUAL_STRING("a1b2c3d4e5f6g7h8i9j10k", result.c_str());
+}
+
+void test_FormatSchemeElevenPlaceholdersOverflow() {
+  constexpr auto scheme = FormatScheme{"a{}b{}c{}d{}e{}f{}g{}h{}i{}j{}k{}l"};
+  static_assert(scheme.overflow);
+
+  auto result = Format(scheme, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
+  TEST_ASSERT_EQUAL_STRING("a{}b{}c{}d{}e{}f{}g{}h{}i{}j{}k{}l OVERFLOW",
+                           result.c_str());
+}
+
+void test_FormatToRuntimeFormatSource() {
+  auto out = std::string{"prefix:"};
+  auto format = std::string{"{}"};
+  auto scheme = FormatScheme{std::string_view{format}};
+  FormatTo(out, scheme, std::string(128, 'x'));
+  auto expected = std::string{"prefix:"} + std::string(128, 'x');
+  TEST_ASSERT_EQUAL_STRING(expected.c_str(), out.c_str());
 }
 }  // namespace ae::test_format_str
 
@@ -53,5 +189,11 @@ int test_format_str() {
   RUN_TEST(ae::test_format_str::test_BracketInTheBegin);
   RUN_TEST(ae::test_format_str::test_BracketInTheEnd);
   RUN_TEST(ae::test_format_str::test_BracketEscape);
+  RUN_TEST(ae::test_format_str::test_MissingArgsAndLiteralNul);
+  RUN_TEST(ae::test_format_str::test_FormatTo);
+  RUN_TEST(ae::test_format_str::test_FormatSchemeRuntimeAndOverflow);
+  RUN_TEST(ae::test_format_str::test_FormatSchemeTenPlaceholdersFit);
+  RUN_TEST(ae::test_format_str::test_FormatSchemeElevenPlaceholdersOverflow);
+  RUN_TEST(ae::test_format_str::test_FormatToRuntimeFormatSource);
   return UNITY_END();
 }
--- tests/test-format/test-format-time.cpp
+++ tests/test-format/test-format-time.cpp
@@ -16,37 +16,146 @@
 
 #include <unity.h>
 
-#include <list>
 #include <chrono>
+#include <cstdint>
+#include <list>
+#include <limits>
 
 #include "aether-miscpp/format/format.h"
 
 namespace ae::test_format_time {
-using TimePoint = std::chrono::time_point<std::chrono::system_clock, std::chrono::milliseconds>;
+using TimePoint =
+    std::chrono::time_point<std::chrono::system_clock, std::chrono::milliseconds>;
+using SteadyTimePoint =
+    std::chrono::time_point<std::chrono::steady_clock, std::chrono::milliseconds>;
+using UnsupportedSteadyTimePoint =
+    std::chrono::time_point<std::chrono::steady_clock, std::chrono::seconds>;
+
+template <typename T>
+concept HasStringFormatter = requires(ae::Formatter<T> formatter, T const& value,
+                                       ae::FormatContext<
+                                           ae::format_internal::StringWriter>& ctx) {
+  formatter.Format(value, ctx);
+};
+
+static_assert(HasStringFormatter<TimePoint>);
+static_assert(!HasStringFormatter<UnsupportedSteadyTimePoint>);
+static_assert(!HasStringFormatter<std::chrono::days>);
+static_assert(!HasStringFormatter<std::chrono::weeks>);
+static_assert(!HasStringFormatter<std::chrono::duration<int, std::pico>>);
+static_assert(!HasStringFormatter<std::chrono::duration<int, std::ratio<2>>>);
+static_assert(HasStringFormatter<std::chrono::duration<int, std::ratio<3600>>>);
+
+}  // namespace ae::test_format_time
+
+template <>
+struct ae::Formatter<ae::test_format_time::SteadyTimePoint> {
+  template <typename TStream>
+  void Format(ae::test_format_time::SteadyTimePoint const&,
+              ae::FormatContext<TStream>& ctx) const {
+    ctx.out().write("steady");
+  }
+};
+
+namespace ae::test_format_time {
 
 void test_FormatTimePoint() {
-  auto tp = TimePoint{std::chrono::milliseconds{9913675}};
-  auto tp_str = Format("=:-{:%H:%M:%S}", tp);
-  TEST_ASSERT_EQUAL_STRING("=:-02:45:13.675000", tp_str.data());
+  auto tp =
+      std::chrono::time_point<std::chrono::system_clock, std::chrono::microseconds>{
+          std::chrono::microseconds{1234567}};
+  auto tp_str = Format("{}", tp);
+  TEST_ASSERT_EQUAL_STRING("1970-01-01 00:00:01.234567", tp_str.data());
+  auto tp_time_str = Format("{:time}", tp);
+  TEST_ASSERT_EQUAL_STRING("00:00:01.234567", tp_time_str.data());
+  auto tp_unknown_str = Format("{time}", tp);
+  TEST_ASSERT_EQUAL_STRING("1970-01-01 00:00:01.234567", tp_unknown_str.data());
 
   auto tp2 = TimePoint{std::chrono::milliseconds{9913675}};
-  auto tp2_str = Format("[{:%H:%M:%S}]", tp2);
-  TEST_ASSERT_EQUAL_STRING("[02:45:13.675000]", tp2_str.data());
+  auto tp2_str = Format("[{}]", tp2);
+  TEST_ASSERT_EQUAL_STRING("[1970-01-01 02:45:13.675000]", tp2_str.data());
+  auto tp2_time_str = Format("{:time}", tp2);
+  TEST_ASSERT_EQUAL_STRING("02:45:13.675000", tp2_time_str.data());
+
+  auto steady_str = Format("{}", SteadyTimePoint{});
+  TEST_ASSERT_EQUAL_STRING("steady", steady_str.data());
+}
+
+void test_FormatNanosecondTimePointTruncatesToMicros() {
+  auto tp =
+      std::chrono::time_point<std::chrono::system_clock, std::chrono::nanoseconds>{
+          std::chrono::nanoseconds{1234567890}};
+
+  auto tp_str = Format("{}", tp);
+  TEST_ASSERT_EQUAL_STRING("1970-01-01 00:00:01.234567", tp_str.data());
+
+  auto tp_time_str = Format("{:time}", tp);
+  TEST_ASSERT_EQUAL_STRING("00:00:01.234567", tp_time_str.data());
 }
 
 void test_FormatListOfTimePoint() {
   auto tp_list = std::list{TimePoint{std::chrono::milliseconds{13675}},
                            TimePoint{std::chrono::milliseconds{14000}},
                            TimePoint{std::chrono::milliseconds{15888}}};
-  auto tp_list_str = Format("{:o=%S}", tp_list);
-  TEST_ASSERT_EQUAL_STRING("o=13.675000, o=14.000000, o=15.888000",
+  auto tp_list_str = Format("{}", tp_list);
+  TEST_ASSERT_EQUAL_STRING("1970-01-01 00:00:13.675000, 1970-01-01 00:00:14.000000, 1970-01-01 00:00:15.888000",
                            tp_list_str.data());
+}
+
+void test_Duration() {
+  TEST_ASSERT_EQUAL_STRING("1:00:00", Format("{}", std::chrono::hours{1}).data());
+  TEST_ASSERT_EQUAL_STRING("15:00:00", Format("{}", std::chrono::hours{15}).data());
+  TEST_ASSERT_EQUAL_STRING("25:00:00", Format("{}", std::chrono::hours{25}).data());
+  TEST_ASSERT_EQUAL_STRING("100:00:00", Format("{}", std::chrono::hours{100}).data());
+  TEST_ASSERT_EQUAL_STRING("1:01:00", Format("{}", std::chrono::minutes{61}).data());
+  TEST_ASSERT_EQUAL_STRING("25:01:00", Format("{}", std::chrono::minutes{1501}).data());
+  TEST_ASSERT_EQUAL_STRING("0:01:01", Format("{}", std::chrono::seconds{61}).data());
+  TEST_ASSERT_EQUAL_STRING("25:01:01", Format("{}", std::chrono::seconds{90061}).data());
+  TEST_ASSERT_EQUAL_STRING("0:00:00.001", Format("{}", std::chrono::milliseconds{1}).data());
+  TEST_ASSERT_EQUAL_STRING("25:01:01.002", Format("{}", std::chrono::milliseconds{90061002}).data());
+  auto d = std::chrono::microseconds{-3723004005LL};
+  auto s = Format("{}", d);
+  TEST_ASSERT_EQUAL_STRING("-1:02:03.004005", s.data());
+  TEST_ASSERT_EQUAL_STRING("25:01:01.002003004",
+                           Format("{}", std::chrono::nanoseconds{90061002003004LL}).data());
+
+  using TwoHourDuration = std::chrono::duration<int, std::ratio<3600>>;
+  TEST_ASSERT_EQUAL_STRING("25:00:00", Format("{}", TwoHourDuration{25}).data());
+
+  auto min_s = Format("{}", std::chrono::microseconds::min());
+  TEST_ASSERT_EQUAL_STRING("-2562047788:00:54.775808", min_s.data());
+
+  using Int8Millis = std::chrono::duration<int8_t, std::milli>;
+  using Int16Micros = std::chrono::duration<int16_t, std::micro>;
+  using Int16Nanos = std::chrono::duration<int16_t, std::nano>;
+  using Int32Micros = std::chrono::duration<int32_t, std::micro>;
+  using Int32Nanos = std::chrono::duration<int32_t, std::nano>;
+  TEST_ASSERT_EQUAL_STRING("0:00:00.127", Format("{}", Int8Millis{127}).data());
+  TEST_ASSERT_EQUAL_STRING("-0:00:00.128",
+                           Format("{}", Int8Millis{std::numeric_limits<int8_t>::min()}).data());
+  TEST_ASSERT_EQUAL_STRING("0:00:00.032767",
+                           Format("{}", Int16Micros{std::numeric_limits<int16_t>::max()}).data());
+  TEST_ASSERT_EQUAL_STRING("-0:00:00.032768",
+                           Format("{}", Int16Micros{std::numeric_limits<int16_t>::min()}).data());
+  TEST_ASSERT_EQUAL_STRING("0:00:00.000032767",
+                           Format("{}", Int16Nanos{std::numeric_limits<int16_t>::max()}).data());
+  TEST_ASSERT_EQUAL_STRING("-0:00:00.000032768",
+                           Format("{}", Int16Nanos{std::numeric_limits<int16_t>::min()}).data());
+  TEST_ASSERT_EQUAL_STRING("0:00:02.147483647",
+                           Format("{}", Int32Nanos{std::numeric_limits<int32_t>::max()}).data());
+  TEST_ASSERT_EQUAL_STRING("-0:00:02.147483648",
+                           Format("{}", Int32Nanos{std::numeric_limits<int32_t>::min()}).data());
+  TEST_ASSERT_EQUAL_STRING("0:35:47.483647",
+                           Format("{}", Int32Micros{std::numeric_limits<int32_t>::max()}).data());
+  TEST_ASSERT_EQUAL_STRING("-0:35:47.483648",
+                           Format("{}", Int32Micros{std::numeric_limits<int32_t>::min()}).data());
 }
 }  // namespace ae::test_format_time
 
 int test_format_time() {
   UNITY_BEGIN();
   RUN_TEST(ae::test_format_time::test_FormatTimePoint);
+  RUN_TEST(ae::test_format_time::test_FormatNanosecondTimePointTruncatesToMicros);
   RUN_TEST(ae::test_format_time::test_FormatListOfTimePoint);
+  RUN_TEST(ae::test_format_time::test_Duration);
   return UNITY_END();
 }
